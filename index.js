02f95b0fad64a90662cf6f2162441d899e687927
Bluefeniz <flaviofrancardi@libero.it>
2022-04-17 01:55:57 +0200

    pushata nuova version

---
+++ index.js
@@ -0,0 +1,11 @@
+const numero = 3;
+
+if (numero < 3){
+    console.log("il numero e' minore di 3");
+}
+elseif(numero == 3){
+    console.log("il numero e' minore di 3");
+}
+else{
+    console.log("il numero e' minore di 3");
+}
