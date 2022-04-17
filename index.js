09dadddfce9bf6b0dd1c9bea7eebece1f371817f
Bluefeniz <flaviofrancardi@libero.it>
2022-04-17 04:08:09 +0200

    rimesso tutto ok

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ const saluta = require('./utils')
 const nomi = require('./nomi')
 console.log(nomi)
 
-<<<<<<< HEAD
-if (numero < 3){
-    console.log("il numero e' minore di 3");
-}
-elseif(numero == 3){
-    console.log("il numero e' minore di 3");
-}
-else{
-    console.log("il numero e' minore di 3");
-}
-=======
 console.log(nomi.persona3)
 saluta(nomi.persona3)
 
@@ -21,4 +10,3 @@ const path = require('path')
 const percorsoFile = path.join('/cartella', 'sottocartella', 'prova.txt');
 const percorsoAssoluto = path.resolve(__dirname, 'cartella', 'sottocartella', 'prova.txt')
 console.log(percorsoAssoluto)
->>>>>>> DEV
