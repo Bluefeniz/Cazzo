4cf0dcb45e49d14d8ad1045524f24a568db812b4
Bluefeniz <flaviofrancardi@libero.it>
2022-04-17 03:47:54 +0200

    pushato fino a lez 7

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const numero = 3;
+const saluta = require('./utils')
+const nomi = require('./nomi')
+console.log(nomi)
 
-if (numero < 3){
-    console.log("il numero e' minore di 3");
-}
-elseif(numero == 3){
-    console.log("il numero e' minore di 3");
-}
-else{
-    console.log("il numero e' minore di 3");
-}
-tre
+console.log(nomi.persona3)
+saluta(nomi.persona3)
+
+const path = require('path')
+
+const percorsoFile = path.join('/cartella', 'sottocartella', 'prova.txt');
+const percorsoAssoluto = path.resolve(__dirname, 'cartella', 'sottocartella', 'prova.txt')
+console.log(percorsoAssoluto)
